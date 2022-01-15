9946fae3a5702e88b3f0d38b32a94338e2f301f9
Carlos <carlos-84@live.com>
2022-01-15 20:11:23 -0300

    Consumindo api

---
+++ ICepApiService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace ProjetoRefit.Models
+{
+    internal interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<Endereco> GetAddresAsync(string cep);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using ProjetoRefit.Models;
+using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace ProjetoRefit
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("https://viacep.com.br");
+                Console.WriteLine("Informe seu cep");
+
+                string cep = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando cep Informado: " + cep);
+
+                var endereco = await cepClient.GetAddresAsync(cep);
+
+                Console.WriteLine("Logradou: " + endereco.Logradouro);
+                Console.WriteLine("Bairro: " + endereco.Bairro);
+                Console.WriteLine("Localidade: " + endereco.Localidade);
+               
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
